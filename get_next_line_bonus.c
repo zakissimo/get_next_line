12cd3a950bf02c7ef908b34a0acedb423c43188d
zakissimo <zakaria.habri@gmail.com>
2022-10-10 15:38:59 +0200

    up

---
+++ get_next_line_bonus.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zhabri <zhabri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 08:47:44 by zhabri            #+#    #+#             */
+/*   Updated: 2022/10/10 15:37:30 by zhabri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+#include <stdlib.h>
+
+static char	*get_out(char **stash, int ret)
+{
+	char		*out;
+	char		**tab;
+
+	if ((!ret && nl_in_str(*stash) == -1)
+		|| (nl_in_str(*stash) == ft_strlen(*stash)))
+	{
+		out = ft_strdup(*stash);
+		*stash = NULL;
+		return (out);
+	}
+	if (nl_in_str(*stash) != -1)
+	{
+		tab = malloc(sizeof(char *) * 2);
+		if (!tab)
+			return (NULL);
+		tab = split_to_tab(tab, nl_in_str(*stash), *stash);
+		*stash = tab[0];
+		out = tab[1];
+		free(tab);
+		return (out);
+	}
+	return (NULL);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	**stash = NULL;
+	int			ret;
+	char		*buf;
+
+	stash = malloc(sizeof(char *) * 1024);
+	if (BUFFER_SIZE <= 0 || fd < 0 || read(fd, (void *)0, 0) == -1)
+		return (NULL);
+	ret = 1;
+	while (ret && nl_in_str(stash[fd]) == -1)
+	{
+		buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!buf)
+			return (NULL);
+		ret = read(fd, buf, BUFFER_SIZE);
+		if ((!ret && !stash[fd]) || ret == -1)
+		{
+			free(buf);
+			return (NULL);
+		}
+		buf[ret] = '\0';
+		stash[fd] = ft_rejoin(stash[fd], buf);
+	}
+	return (get_out(&stash[fd], ret));
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zhabri <zhabri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 08:27:00 by zhabri            #+#    #+#             */
+/*   Updated: 2022/10/10 15:30:53 by zhabri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+typedef struct s_node
+{
+	char	*stash;
+	char	*out;
+}			t_node;
+
+char		*get_next_line(int fd);
+int			nl_in_str(char *str);
+int			ft_strlen(char *str);
+char		*ft_strdup(char *buf);
+char		*ft_rejoin(char *stash, char *buf);
+char		**split_to_tab(char **tab, int nl_idx, char *stash);
+char		*ft_rejoin(char *stash, char *buf);
+
+#endif
